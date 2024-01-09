6f2756912894f19e72d1fb30740544a9a7cdb303
Moksleivis <egidijus.navickas@stud.kit.lt>
2024-01-09 15:19:28 +0200

    a

--- src/utilities/validate.jsx
+++ src/utilities/validate.jsx
@@ -1,3 +1,16 @@
-// export default function App(input){
-//     return input
-// }
+import {useState} from 'react';
+
+export default function validate(){
+    const [message, setMessage] = useState('');
+    const handleChange = event => {
+        setMessage(event.target.value);
+    
+        if (event.target.value.trim().length > 0) {
+          console.log('✅ Input is not empty');
+        } else {
+          console.log('⛔️ Input is empty');
+        }
+      };
+
+    return 
+}
